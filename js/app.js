02aa6d202b95ab2b1e47c6c9b9efb4f00b91cdca
Ricky Garcia <garcrich@outlook.com>
2015-12-20 22:41:18 -0700

    added functionality to app.js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,87 @@
-// Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    //sets initial run of Enemy
+    this.x = -100;
+    this.yRange = [211, 129, 47];
+    this.y = this.yRange[Math.floor(Math.random() * 3)];
+    this.speed = 150 * Math.random() + 100; //,200,300;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    //repeats initial run
+    this.x += this.speed * dt;
+    if (this.x > 550) {
+        this.x = -100;
+        this.speed = 100 * Math.floor(Math.random() * 3) + 100;
+        this.y = this.yRange[Math.floor(Math.random() * 3)];
+    }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var player = function() {
+    //starting location
+    this.x = 205;
+    this.y = 375;
+
+    this.sprite = 'images/char-boy.png';
+};
+
+player.prototype.reset = function() {
+    //fall back to starting location
+    this.x = 205;
+    this.y = 375;
+};
+
+//code from https://github.com/walesmd/uda-frogger/blob/master/app.js
+player.prototype.update = function() {
+    //simply executes checkCollisions function
+    this.checkCollisions();
+};
+
+player.prototype.checkCollisions = function() {
+    if (this.y <= -0) {
+        this.reset();
+    } else if (this.y >= 40 && this.y <= 220) {
+        var self = this;
+        // player is on road rows, check collisions
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+        //code modified from https://github.com/walesmd/uda-frogger/blob/master/app.js
+        // loop through each bug
+        allEnemies.forEach(function(bug) {
+            // is the bug on the same row as the player?
+            if (bug.y == self.y) {
+                // is the bug on the player?
+                //code modified to wider radius for more challenging play
+                if (bug.x >= player.x - 40 && bug.x <= player.x + 40) {
+                    self.reset();
+                }
+            }
+        });
+    }
+};
 
+player.prototype.handleInput = function(keyup) {
+//direct player with directional buttons
 
+    if (keyup == 'left' && this.x >= 10) {
+        this.x -= 101;
+    } else if (keyup == 'right' && this.x <= 400) {
+        this.x += 101;
+    } else if (keyup == 'up') {
+        this.y -= 82;
+    } else if (keyup == 'down' && this.y <= 370) {
+        this.y += 82;
+    }
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function(event) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -42,5 +89,22 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[event.keyCode]);
 });
+
+// Draw the enemy on the screen, required method for game
+player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+
+
+
+//create three enemies using the new operator
+var allEnemies = [Bug1 = new Enemy(), Bug2 = new Enemy(), Bug3 = new Enemy()];
+// Place the player object in a variable called player
+
+//create player useing the new operator
+var player = new player();
